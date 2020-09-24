5d0864dd7fdf043d1c7861a06059fe6646a039f7
mkshuvo <mkshuvo@hotmail.com>
2020-09-24 21:19:09 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
